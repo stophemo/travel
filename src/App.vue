c7fd20f2dbec7a423c35ca6a6f771e679acb78e0
Clover996 <clover113@163.com>
2021-11-15 00:16:24 +0800

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view />
+    <!-- 显示的是当前路由地址所对应的内容 -->
   </div>
 </template>
 
@@ -13,7 +13,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+// 1像素边框解决方案
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
@@ -13,3 +18,5 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+// 路由就是根据不同的网址  返回不同的内容给用户
